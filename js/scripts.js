227125a6955aa10a4d834496f4ee2870d4e25beb
JorgeHerter <159959563+JorgeHerter@users.noreply.github.com>
2024-03-01 18:39:10 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+//1. Adding new values to the list
 let li = $('<li></li>');
 let inputValue = $('#input').val();
 li.append(inputValue);
@@ -7,7 +9,7 @@ if (inputValue === ' ') {
 } else {
     $('#list').append(li);
 }
-
+//2.Crossing out an Item
 function crossOut() {
     li.toggleClass("strike");
 }
@@ -15,7 +17,7 @@ function crossOut() {
 li.on("dbclick", function crossout() {
     li.toggleClass("strike");
 });
-
+//3. adding a delete button
 let crossOutButton = $('<crossOutButton></crossOutButton>');
 crossOutButton.append(document.createTextNode('X'));
 li.append(crossOutButton);
@@ -24,4 +26,5 @@ crossOutButton.on("click", deleteListItem);
 function deleteListItem() {
     li.addClass("delete")
 }
+//4. reordering the items
 $('#list').sortable();
